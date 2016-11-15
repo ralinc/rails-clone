c5a46a2215b58c6b75757a4bc3208ef44e4ecbf5
Ralin Chimev <ralin.chimev@gmail.com>
2016-11-15 22:01:53 +0200

    Generate the rails-clone gem using bundler
    `$ bundler gem rails-clone`

---
+++ lib/rails/clone.rb
@@ -0,0 +1,7 @@
+require "rails/clone/version"
+
+module Rails
+  module Clone
+    # Your code goes here...
+  end
+end
---
+++ lib/rails/clone/version.rb
@@ -0,0 +1,5 @@
+module Rails
+  module Clone
+    VERSION = "0.1.0"
+  end
+end
