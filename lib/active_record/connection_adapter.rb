d5bafb555a46516e57f8bed80e14c862e4d4ab46
Ralin Chimev <ralin.chimev@gmail.com>
2016-12-11 16:06:50 +0200

    Clean rubocop warnings

--- lib/active_record/connection_adapter.rb
+++ lib/active_record/connection_adapter.rb
@@ -2,7 +2,7 @@ module ActiveRecord
   module ConnectionAdapter
     class SqliteAdapter
       def initialize(file)
-        require "sqlite3"
+        require 'sqlite3'
         @db = SQLite3::Database.new(file.to_s, results_as_hash: true)
       end
 
@@ -10,15 +10,13 @@ module ActiveRecord
         @db.execute(sql).each do |row|
           row.keys.each do |key|
             value = row.delete(key)
-            if key.is_a? String
-              row[key.to_sym] = value
-            end
+            row[key.to_sym] = value if key.is_a? String
           end
         end
       end
 
       def columns(table_name)
-        @db.table_info(table_name).map { |info| info["name"].to_sym }
+        @db.table_info(table_name).map { |info| info['name'].to_sym }
       end
     end
   end
